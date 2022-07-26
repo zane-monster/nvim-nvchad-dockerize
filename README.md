11eb944cd0caf863f3099bc1e182178ccca184cb
zane <zane_n0002@ie8.com>
2022-07-26 13:20:49 +1200

    first

---
+++ README.md
@@ -0,0 +1,108 @@
+# docker compose lifecycle
+
+1. start docker compose services
+
+```sh
+docker-compose up
+```
+
+then, the nvim container is creaded and in exited status.
+
+2. connect to the service nvim in the first term
+
+```sh
+docker-compose run nvim bash
+```
+
+the nvim container is running, now.
+
+3. connect to the container in other terms
+
+```sh
+# get the id of nvim container
+docker ps -a
+docker exec -it <the nvim container id> bash
+
+# Invalid command in windows ???
+docker-compose exec nvim bash
+docker-compose exec --entrypoint /bin/bash nvim
+```
+
+4. exit in the first term and docker rm ...
+
+# docker lifecycle
+
+1. start a new container in the first term
+
+```sh
+docker run -it -p 3000:3000 -p 4000:4000 -p 8000:8000 -p 9000:9000 -v c:\a:/home/nv/a -v c:\Users\username\.ssh:/home/nv/.ssh -v "//var/run/docker.sock:/var/run/docker.sock" zane/nvchad
+
+sudo chmod 600 /home/nv/.ssh/config
+```
+
+2. start a exited container
+
+```sh
+docker start -ai <exited container id>
+```
+
+3. connect to the running container
+
+```sh
+docker exec -it <running container id> bash
+
+# as root
+docker exec -u 0  -it <running container id> bash
+```
+
+4. exit in the first term and docker rm ...
+
+# buld docker image
+
+1. update the base image
+
+```sh
+docker pull archlinux:base-devel
+# list images
+docker images
+# clean old images
+docker image rm a570
+```
+
+2. build the image
+
+```sh
+docker build -f Dockerfile.nvchad.build -t zane/nvchad .
+```
+
+# After a contain first running
+
+## init nvim
+
+1. start nvim, then :qa
+2. start nvim, then :qa
+3. start nvim, then :GoInstallBinaries, then :qa
+4. start nvim, then open a go file, then :qa
+5. start nvim, then :checkhealth
+
+## updat nvchad
+
+```
+<lead> + u + u
+```
+
+# arch linux notes
+
+## set mirrorlist
+
+```sh
+# in docker # as root
+reflector -f 5 --country 'United States' --protocol https --sort rate  --download-timeout 2 --save /etc/pacman.d/mirrorlist
+
+reflector -f 5 --country 'United States' \
+--protocol https \
+--sort rate \
+--download-timeout 2 \
+--completion-percent 100 \
+--save /etc/pacman.d/mirrorlist
+```
---
+++ nvchad/custom/plugins/init.lua
@@ -0,0 +1,111 @@
+return {
+
+    ["vimwiki/vimwiki"] = {
+        config = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = 'G:\\My Drive\\Document_backup\\vimwiki\\',
+                    syntax = 'markdown',
+                    ext = '.md'
+                }
+            }
+        end
+    },
+
+    ["nvim-telescope/telescope.nvim"] = {module = "telescope"},
+    -- golang settings BEGIN ---
+    ["mfussenegger/nvim-dap"] = {
+        -- config = function() require("nvim-dap").setup() end
+    },
+    ["rcarriga/nvim-dap-ui"] = {
+        -- config = function() require("nvim-dap-ui").setup() end
+    },
+    ["theHamsta/nvim-dap-virtual-text"] = {
+        config = function() require("nvim-dap-virtual-text").setup() end
+    },
+    ["nvim-telescope/telescope-dap.nvim"] = {
+        after = {
+            "telescope.nvim", "nvim-dap", "nvim-dap-ui", "nvim-dap-virtual-text"
+        },
+        config = function() require("telescope").load_extension "dap" end
+    },
+
+    ["ray-x/guihua.lua"] = {
+        config = function() os.execute("cd lua/fzy && make") end
+    },
+
+    ["ray-x/navigator.lua"] = {
+        after = {"guihua.lua", "nvim-lspconfig"},
+        config = function() require("navigator").setup() end
+    },
+
+    ["ray-x/go.nvim"] = {config = function() require "custom.plugins.go" end},
+    -- golang settings END
+
+    ["ojroques/vim-oscyank"] = {
+        config = function()
+            if vim.env.SSH_CLIENT or vim.env.SSH_TTY then
+                vim.cmd("autocmd!")
+                vim.cmd("autocmd TextYankPost * OSCYankReg +<CR>")
+            end
+        end
+    },
+
+    ["windwp/nvim-ts-autotag"] = {
+        ft = {"html", "javascriptreact"},
+        after = "nvim-treesitter",
+        config = function()
+            local present, autotag = pcall(require, "nvim-ts-autotag")
+
+            if present then autotag.setup() end
+        end
+    },
+
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+        after = "nvim-lspconfig",
+        config = function() require "custom.plugins.null-ls" end
+    },
+
+    ["nvim-telescope/telescope-media-files.nvim"] = {
+        after = "telescope.nvim",
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    media_files = {filetypes = {"png", "webp", "jpg", "jpeg"}}
+                    -- fd is needed
+                }
+            }
+            require("telescope").load_extension "media_files"
+        end
+    },
+
+    ["Pocco81/TrueZen.nvim"] = {
+        cmd = {"TZAtaraxis", "TZMinimalist", "TZFocus"},
+        config = function() require "custom.plugins.truezen" end
+    },
+
+    ["nvim-neorg/neorg"] = {
+        ft = "norg",
+        after = "nvim-treesitter",
+        config = function() require "custom.plugins.neorg" end
+    },
+
+    ["nvim-treesitter/playground"] = {
+        cmd = "TSCaptureUnderCursor",
+        config = function() require "nvim-treesitter.configs" end
+    },
+
+    ["simrat39/symbols-outline.nvim"] = {},
+
+    ["nvim-lua/plenary.nvim"] = {},
+    ["sindrets/diffview.nvim"] = {
+        after = "plenary.nvim"
+        -- config = function()
+        --    require("custom.plugins.diffview").setup()
+        -- end,
+    },
+
+    ["Pocco81/AutoSave.nvim"] = {
+        config = function() require "custom.plugins.autosave" end
+    }
+}
